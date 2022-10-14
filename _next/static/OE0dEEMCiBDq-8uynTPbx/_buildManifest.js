ecfe11c4a1102c967baf0231019187e9c2b04e66
Tahera B <4462484+taheraab@users.noreply.github.com>
2022-10-14 16:21:46 +0200

    Updates

---
+++ _next/static/OE0dEEMCiBDq-8uynTPbx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ee91491f550d3456.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],"/about":["static/chunks/pages/about-46ab272689f0b726.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
